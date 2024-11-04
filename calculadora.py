77957c209fd683ec11fc5169fe521d2ed244e19f
Jonathan David Duran Ortiz <l22121053@morelia.tecnm.mx>
2024-11-04 14:51:38 -0600

    Tarea 1 Unidad 5

---
+++ calculadora.py
@@ -0,0 +1,67 @@
+import tkinter as tk
+from tkinter import messagebox
+ 
+def sumar():
+    try:
+        num1 = float(entry_num1.get())
+        num2 = float(entry_num2.get())
+        suma = num1 + num2
+        messagebox.showinfo("Resultado", f"La suma es: {suma}")
+    except ValueError:
+        messagebox.showerror("Error", "Por favor, ingresa números válidos.")
+
+
+def restar():
+    try:
+        num1 = float(entry_num1.get())
+        num2 = float(entry_num2.get())
+        resta = num1 - num2
+        messagebox.showinfo("Resultado", f"La resta es: {resta}")
+    except ValueError:
+        messagebox.showerror("Error", "Por favor, ingresa números válidos.")
+
+def dividir():
+    try:
+        num1 = float(entry_num1.get())
+        num2 = float(entry_num2.get())
+        division = num1/num2
+        messagebox.showinfo("Resultado", f"La division es: {division}")
+    except ValueError:
+        messagebox.showerror("Error", "Por favor, ingresa números válidos.")
+
+def multiplicar():
+    try:
+        num1 = float(entry_num1.get())
+        num2 = float(entry_num2.get())
+        multi = num1*num2
+        messagebox.showinfo("Resultado", f"La multiplicacion es: {multi}")
+    except ValueError:
+        messagebox.showerror("Error", "Por favor, ingresa números válidos.")
+ 
+ 
+ventana = tk.Tk()
+ventana.title("Calculadora")
+ventana.geometry("300x200")
+ 
+label_num1 = tk.Label(ventana, text="Número 1:")
+label_num1.pack(pady=5)
+entry_num1 = tk.Entry(ventana)
+entry_num1.pack(pady=5)
+ 
+label_num2 = tk.Label(ventana, text="Número 2:")
+label_num2.pack(pady=5)
+entry_num2 = tk.Entry(ventana)
+entry_num2.pack(pady=5)
+ 
+boton_sumar = tk.Button(ventana, text="Sumar", command=sumar)
+boton_sumar.pack(pady=20)
+
+boton_restar= tk.Button(ventana, text="Restar", command=restar)
+boton_restar.pack(pady=20)
+
+boton_dividir= tk.Button(ventana, text="Division", command=dividir)
+boton_dividir.pack(pady=20)
+ 
+boton_multiplicar= tk.Button(ventana, text="Multiplicacion", command=multiplicar)
+boton_multiplicar.pack(pady=20)
+ventana.mainloop()
